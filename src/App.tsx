1abb428e2effa5c379ca6a8f8a9f58eb48983d77
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-11-10 23:14:34 +0600

    Installed Vite, Tailwind,DaisyUI, Shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Md.Ashraful Haque's Portfolio Website</h1>
+      <Button>Click me baby</Button>
+    </>
+  );
+}
+
+export default App;
